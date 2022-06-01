15138d1580897bfbb76df4f1443e5ffbf98f92b3
DmishK <102250054+DmishK@users.noreply.github.com>
2022-06-01 21:17:42 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    def __init__(self):
+        self.api_key = '1053260ea04691544e46e709b227724d9c7526d6008f0ea0ffe8e4e66007e96r'
+        self.bot_token = '5400860318:AAFaDX9cKTwm5hejDCB4_pfJyHcfT_MIYrw'
